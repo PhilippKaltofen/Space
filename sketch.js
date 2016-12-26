e53026f94e6e56007c06a89a5914f17197c09848
PhilippKaltofen <pkaltofen@yahoo.com>
2016-12-26 16:18:48 +0100

    Rotation, ship visuals, particles
    rotation added, ship looks like a ship now, particles added, background
    added

--- sketch.js
+++ sketch.js
@@ -4,6 +4,12 @@
 
 var player;
 var planets;
+var backgroundLayer_1;
+var backgroundLayer_2;
+
+var particles_rocket_main;
+var particles_rocket_left;
+var particles_rocket_right;
 
 // SETUP FUNCTION
 
@@ -14,8 +20,21 @@ var setup = function(){
 
     player = new rocketShip(width / 2, height / 2);
     planets = [];
-    for(var i = 0; i < 10; i++){
+    for(var i = 0; i < 3; i++){
         planets.push(new planet(random(0, width), random(0, height), random(50, 150)));
+    }
+
+    backgroundLayer_1 = [];
+    backgroundLayer_2 = [];
+
+    for(var i = 0; i < 30; i++){
+        backgroundLayer_1[i] = new backgroundDetail(random(0, width), random(0, height));
+        backgroundLayer_2[i] = new backgroundDetail(random(0, width), random(0, height));
+    }
+
+    particles_rocket_main = [];
+    for(var i = 0; i < 50; i++){
+        particles_rocket_main[i] = new particle(player, player.position.x, player.position.y, 200, 200, 0, random(-1,1), 1, random(5, 10), random(3,10), random(1,3));
     }
 }
 
@@ -24,9 +43,25 @@ var setup = function(){
 var draw = function(){
     background(20,20,20);
 
+    // draw background
+    // back layer
+    for(var i = 0; i < backgroundLayer_2.length; i++){
+        backgroundLayer_2[i].drawDetail();
+    }
+    // front layer
+    for(var i = 0; i < backgroundLayer_1.length; i++){
+        backgroundLayer_1[i].drawDetail();
+    }
+
     // draw planets
     for(var i = 0; i < planets.length; i++){
         planets[i].drawPlanet();
+    }
+
+    // draw engine particles
+    // main engine
+    for(var i = 0; i < particles_rocket_main.length; i++){
+        particles_rocket_main[i].drawParticle();
     }
 
     // draw player
@@ -39,8 +74,7 @@ var planet = function(posX, posY, size){
     this.b = 0;
     this.seed = round(random(0,2));
     this.rings = round(random(0,1));
-    this.posX = posX;
-    this.posY = posY;
+    this.position = createVector(posX, posY);
     this.size = size;
     this.gravity = size / 500;
 
@@ -63,26 +97,26 @@ var planet = function(posX, posY, size){
         noStroke();       
         // planet glow
         fill(this.r, this.g, this.b, 50);
-        ellipse(this.posX, this.posY, this.size * 1.8, this.size * 1.8)        
+        ellipse(this.position.x, this.position.y, this.size * 1.8, this.size * 1.8)        
         // planet base
         fill(this.r, this.g, this.b, 255);
-        ellipse(this.posX, this.posY, this.size, this.size);
+        ellipse(this.position.x, this.position.y, this.size, this.size);
 
         // rings
         if(this.rings === 1){
             stroke(this.r * 1.5, this.g * 1.5, this.b * 1.5,200);
             fill(0,0,0,0);
             strokeWeight(3);
-            ellipse(this.posX, this.posY, this.size * 1.5, this.size / 2);
+            ellipse(this.position.x, this.position.y, this.size * 1.5, this.size / 2);
             noStroke();
             fill(this.r,this.g,this.b, 255);
-            ellipse(this.posX, this.posY - this.size / 5, this.size * 0.9, this.size * 0.2);
+            ellipse(this.position.x, this.position.y - this.size / 5, this.size * 0.9, this.size * 0.2);
         }
 
         // distance to player
         textAlign(CENTER);
-        fill(255);
-        text(round(dist(this.posX, this.posY, player.posX, player.posY)), this.posX, this.posY);
+        fill(255, 255, 255);
+        text(round(dist(this.position.x, this.position.y, player.position.x, player.position.y)), this.position.x, this.position.y);
 
         if(player.nearestPlanet == this){
             this.r = 255;
@@ -93,38 +127,155 @@ var planet = function(posX, posY, size){
 }
 
 var rocketShip = function(posX, posY){
-    this.posX = posX;
-    this.posY = posY;
+    this.position = createVector(posX, posY);
     this.nearestPlanetDistance = 100000;
     this.nearestPlanet = null;
     this.velocityX = 1;
     this.velocityY = 1;
+    this.force = 1;
+    this.rotation = 0;
 
     this.drawRocketShip = function(){
+        // draw rocketship
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(degrees(this.rotation));
+
+        // rocketship base
         fill(200,200,200);
-        rect(this.posX, this.posY, 20, 50);
+        rect(0, 0, 20, 40);
+        // top
+        ellipse(0, -20, 20, 20);
+        // wings
+        fill(150,150,150);
+        triangle(-10, 10, -10, -20, -20, 10);        
+        triangle(10, 10, 10, -20, 20, 10);
+        // window
+        fill(20, 20, 20);
+        ellipse(0, -18, 15, 15);
+        fill(200, 200, 200);
+        rect(0, -10, 15, 10);
+        // ENGINES
+        // main
+        fill(150,150,150);
+        beginShape();
+        vertex(-5, 20);
+        vertex(5, 20);
+        vertex(8, 25);
+        vertex(-8, 25);
+        endShape(CLOSE);
+        // left
+        fill(200,200,200);
+        rect(-14, 12, 6, 5);
+        // right
+        fill(200,200,200);
+        rect(14, 12, 6, 5);
+
+        pop();
 
         // let gravity of planets affect ship
         // nearest planet
 
         for(var i = 0; i < planets.length; i++){
-            if(dist(planets[i].posX, planets[i].posY, this.posX, this.posY) < this.nearestPlanetDistance && planets[i] != this.nearestPlanet){
+            if(dist(planets[i].position.x, planets[i].position.y, this.position.x, this.position.y) < this.nearestPlanetDistance && planets[i] != this.nearestPlanet){
                 this.nearestPlanet = planets[i];
-                this.nearestPlanetDistance = dist(this.nearestPlanet.posX, this.nearestPlanet.posY, this.posX, this.posY);
+                this.nearestPlanetDistance = dist(this.nearestPlanet.position.x, this.nearestPlanet.position.y, this.position.x, this.position.y);
             }
         }
 
-        this.nearestPlanetDistance = dist(this.nearestPlanet.posX, this.nearestPlanet.posY, this.posX, this.posY);
+        this.nearestPlanetDistance = dist(this.nearestPlanet.position.x, this.nearestPlanet.position.y, this.position.x, this.position.y);
 
-        var dx = (this.nearestPlanet.posX - this.posX);
-        var dy = (this.nearestPlanet.posY - this.posY);
+        var dx = (this.nearestPlanet.position.x - this.position.x);
+        var dy = (this.nearestPlanet.position.y - this.position.y);
 
-        this.posX += this.velocityX;
-        this.posY += this.velocityY;
-        this.posX += dx * (this.nearestPlanet.gravity / this.nearestPlanetDistance);
-        this.posY += dy * (this.nearestPlanet.gravity / this.nearestPlanetDistance);
+        // this.force.add(1 * (this.nearestPlanet.position.x - this.position.x) / this.nearestPlanetDistance ^ 2, 1 * (this.nearestPlanet.position.y - this.position.y) / this.nearestPlanetDistance ^ 2);
+
+        textAlign(LEFT);
+        text("Direction: " + this.position.heading(), width / 2, 20);
+        text("Magnitude: " + this.position.mag(), width / 2, 40);
+
+        if(keyIsPressed){
+            this.force++;
+        }
+
+        // this.position.add(0.1 * this.position.heading() * this.force);
+        // this.position.x += dx * (this.nearestPlanet.gravity / this.nearestPlanetDistance);
+        // this.position.y += dy * (this.nearestPlanet.gravity / this.nearestPlanetDistance);
 
         stroke(200);
-        line(this.posX, this.posY, this.nearestPlanet.posX, this.nearestPlanet.posY);
+        line(this.position.x, this.position.y, this.nearestPlanet.position.x, this.nearestPlanet.position.y);
+
+        // INPUT
+
+        if(keyIsDown(87)){
+            // UP
+        } else
+        if(keyIsDown(83)){
+            // DOWN
+        }
+
+        if(keyIsDown(65)){
+            this.rotation -= 0.001;
+        } else
+        if(keyIsDown(68)){
+            this.rotation += 0.001;
+        }
     }
+}
+
+var backgroundDetail = function(posX, posY){
+    this.position = createVector(posX, posY);
+    this.r = 200;
+    this.g = 200;
+    this.b = 0;
+    this.size = random(0, 5);
+
+    this.drawDetail = function(){
+        // fill(this.r, this.g, this.b);        
+        noStroke();
+        fill(this.r, this.g, this.b);
+        ellipse(this.position.x, this.position.y, this.size, this.size);
+    }
+}
+
+var particle = function(parent, posX, posY, r, g, b, dirX, dirY, decaySpeed, size, speed){
+    this.parent = parent;
+    this.startPosition = createVector(this.parent.position.x, this.parent.position.y + 20);
+    this.position = createVector(posX, posY);
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.opacity;
+    this.size = size;
+    this.direction = createVector(dirX, dirY);
+    this.moving = true;
+    this.fade = true;
+    this.decaySpeed = decaySpeed;
+    this.speed = speed;
+
+    this.drawParticle = function(){
+        // draw particle
+        fill(this.r, this.g, this.b, this.opacity);
+        ellipse(this.position.x, this.position.y, this.size, this.size);
+        // move particle
+        if(this.moving){
+            this.position.x += this.direction.x * this.speed;
+            this.position.y += this.direction.y * this.speed;
+        }
+        // fadeout
+        if(this.fade){
+            if(this.opacity > 0){
+                this.opacity -= decaySpeed;
+            } else {
+                this.resetParticle();
+            }
+        }
+    }
+
+    this.resetParticle = function(){
+        this.position.x = this.startPosition.x;
+        this.position.y = this.startPosition.y;
+        this.opacity = 255;
+    }
+
 }
